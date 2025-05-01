e8e19777e9a98ef52b076fe749bbcb86a47941b0
Nmanz <nurmaneil12@gmail.com>
2025-05-01 19:25:05 +0700

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function tampilkanNama() {
     if (nama.trim() === "") {
       document.getElementById("output").textContent = "Silakan masukkan nama terlebih dahulu.";
     } else {
-      document.getElementById("output").textContent = "Selamat pagi, " + nama + " cantik";
+      document.getElementById("output").textContent = "Selamat Ulang Tahun, " + nama + " cantik";
     }
   }
   

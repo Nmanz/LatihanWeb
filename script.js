ae1b650a46e01deca73e0494b3400a3d0ca18db2
Nmanz <nurmaneil12@gmail.com>
2025-05-01 10:24:43 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function ubahTeks() {
-    document.getElementById("pesan").textContent = "Nama saya Ilham";
+function tampilkanNama() {
+    var nama = document.getElementById("inputNama").value;
+  
+    if (nama.trim() === "") {
+      document.getElementById("output").textContent = "Silakan masukkan nama terlebih dahulu.";
+    } else {
+      document.getElementById("output").textContent = "Selamat pagi, " + nama + " cantik";
+    }
   }
   
